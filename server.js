ff54ffcc51249993395680b138f4e060fa5b52ee
Santos Rivera <santosrivera@Santoss-MacBook-Pro.local>
2025-09-08 10:01:06 -0500

    changed back to *

--- server.js
+++ server.js
@@ -471,7 +471,7 @@ const __dirname = path.dirname(__filename);
 const clientBuildPath = path.join(__dirname, 'dist');
 app.use(express.static(clientBuildPath));
 
-app.get(/^\/(?!api).*/, (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
