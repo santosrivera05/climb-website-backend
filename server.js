875fc13b95e7b30e22942c74cc6f7e5e29a3728c
Santos Rivera <santosrivera@Santoss-MacBook-Pro.local>
2025-09-09 19:18:12 -0500

    otp

--- server.js
+++ server.js
@@ -366,7 +366,7 @@ async function sendVerificationEmail(recipient_email, OTP) {
 
 apiRouter.post("/send-recovery-email", async (req, res) => {
   const { recipient_email, pwd, firstName } = req.body;
-  console.log(recipient_email, pwd, firstName);
+  console.log(recipient_email, pwd, firstName, OTP);
 
   try {
     if (!firstName) {
